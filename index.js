09357b1c4231cea71e646aec6c391401ab69d028
Aziz Oumjahde <aziz.dev@gmail.com>
2025-10-09 21:53:31 +0100

    v1.2

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.icon')));
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnofoedTopology: true,
         });
